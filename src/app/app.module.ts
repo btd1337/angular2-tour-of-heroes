e28791266639783e14c0a065c12cce4b93c3de98
btd1337 <helder.bertoldo@ice.ufjf.br>
2017-02-25 23:04:45 -0300

    0.3 Multiple Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { AppComponent }  from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
 import { NgModule }      from '@angular/core';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
