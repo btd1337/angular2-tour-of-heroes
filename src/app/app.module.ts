8f0603544107d3db9607f5a6fb91bc1c6fe0bfad
btd1337 <helder.bertoldo@ice.ufjf.br>
2017-02-25 22:00:40 -0300

    0.2 Master-Detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent }  from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule }      from '@angular/core';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule,
+    FormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
