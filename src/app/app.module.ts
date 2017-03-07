de2b2a560fdb88700fd79085681f67d6c5046c54
btd1337 <helder.bertoldo@ice.ufjf.br>
2017-03-07 19:04:16 -0300

    Http Hero service update method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes.component';
+import { HttpModule } from '@angular/http';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
+
 @NgModule({
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
